1819cc7c8912746d5da8a4c2d519fc23c92a6851
chenchenyu <chenchenyu@thewesthill.net>
2021-02-24 13:08:39 +0800

    vuepress upload

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // title: "飞跃",
+    //   description: "飞跃的笔记",
+    //   head: [
+    //     ['meta',{name: 'author', content: '飞跃'}],
+    //     ['metah',{name: 'keywords', content: 'vuepress 介绍，vuepress说明,飞跃'}],
+    //   ],
+
+    themeConfig: {
+        nav: [  //链接
+          { 
+            text: '文档查看', 
+            ariaLabel:'Language Menu',
+            items:[
+              {text: '手游自动化测试',link:'/UAuto'}
+            ]
+          }
+        ]
+    }
+}
